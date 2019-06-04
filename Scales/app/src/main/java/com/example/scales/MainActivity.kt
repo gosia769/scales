bd56e15109fcf2b8e4ee67013dda5d2e53b5d9ee
Bartosz Radzynski <bradzynski@virtuslab.com>
2019-06-04 09:54:57 +0200

    Fix set data

--- Scales/app/src/main/java/com/example/scales/MainActivity.kt
+++ Scales/app/src/main/java/com/example/scales/MainActivity.kt
@@ -19,37 +19,20 @@ class MainActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         db = Room.databaseBuilder(
             this,
             AppDatabase::class.java, "weightDB"
         ).allowMainThreadQueries().build()
 
-//        db.weightDao().insertAll(
-//            Weight(0, Date(), 55.0),
-//            Weight(1, Date(), 54.0)
-//        )
-
-
-//        val binding: ActivityMainBinding = DataBindingUtil.setContentView<Any>(
-//            this, R.layout.activity_main)
-
         if (db.weightDao().getAll().isNotEmpty()) {
             print(db.weightDao().getAll().size.toString())
-            var lastWeight = db.weightDao().loadAllByIds(IntArray(db.weightDao().getAll().lastIndex)).get(0)
-
-//            binding.weight = Weight(lastWeight.id, lastWeight.date, lastWeight.value)
-
-
-//            findViewById<TextView>(R.id.lastWeightDate).apply {
-//                text = lastWeight.date.toString()
-//            }
-//            findViewById<TextView>(R.id.lastWeightValue).apply {
-//                text = lastWeight.value.toString()
-//            }
+            val lastWeight = db.weightDao().loadAllByIds(IntArray(db.weightDao().getAll().lastIndex))[0]
+            findViewById<TextView>(R.id.lastWeightDate).text = lastWeight.date.toString()
+            findViewById<TextView>(R.id.lastWeightValue).text = lastWeight.value.toString()
         }
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 
     fun onButtonHistoryClick(view: View){
